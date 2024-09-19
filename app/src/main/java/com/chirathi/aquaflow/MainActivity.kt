ea27f0d170207d8952ac03faff7993c4942031dd
Chirathi Somadasa <chirathisomadasa@gmail.com>
2024-09-19 14:29:04 +0530

    Implement Launch Screen

--- app/src/main/java/com/chirathi/aquaflow/MainActivity.kt
+++ app/src/main/java/com/chirathi/aquaflow/MainActivity.kt
@@ -2,10 +2,20 @@ package com.chirathi.aquaflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            // Set the window to fullscreen
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        requestWindowFeature(Window.FEATURE_NO_TITLE)// Request to hide the title bar
+
         setContentView(R.layout.activity_main)
     }
 }
