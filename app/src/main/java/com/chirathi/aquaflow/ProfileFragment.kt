e925ce080a8a415f406129407ad5e05e277826a4
Balasooriya B.M.P.U <piumiudeshika02@gmail.com>
2024-09-20 15:25:41 +0530

    Update Profile

--- app/src/main/java/com/chirathi/aquaflow/ProfileFragment.kt
+++ app/src/main/java/com/chirathi/aquaflow/ProfileFragment.kt
@@ -6,8 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
+import android.widget.RelativeLayout
 
 
 class ProfileFragment : Fragment() {
@@ -16,8 +15,21 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val rootView =  inflater.inflate(R.layout.fragment_profile, container, false)
+
+        // Initialize the profile details layout
+        val profileDetailsLayout:RelativeLayout = rootView.findViewById(R.id.profile_details)
+
+        // Set a click listener on the profile details layout
+        profileDetailsLayout.setOnClickListener {
+            // Start ProfileDetailsActivity
+            val intent = Intent(activity, ProfileDetailsActivity::class.java)
+            startActivity(intent)
+        }
+
+        return rootView
 
     }
 
