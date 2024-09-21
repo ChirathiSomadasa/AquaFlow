d98207407fa6023f573243f79fd320592be3cf12
Balasooriya B.M.P.U <piumiudeshika02@gmail.com>
2024-09-22 00:38:34 +0530

    update popup message

--- app/src/main/java/com/chirathi/aquaflow/MembershipDetailsActivity.kt
+++ app/src/main/java/com/chirathi/aquaflow/MembershipDetailsActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -25,5 +27,42 @@ class MembershipDetailsActivity : AppCompatActivity() {
         back.setOnClickListener {
             finish()
         }
+
+        val collectButton: Button = findViewById(R.id.collect_button)
+
+        collectButton.setOnClickListener {
+            // Create an AlertDialog.Builder
+            val builder = AlertDialog.Builder(this)
+
+            // Inflate the custom layout
+            val inflater = layoutInflater
+            val dialogView = inflater.inflate(R.layout.custome_dialog_congrats, null)
+
+            // Set the custom layout to the dialog
+            builder.setView(dialogView)
+
+            // Create the dialog instance first
+            val dialog: AlertDialog = builder.create()
+
+            // Get the views from the custom layout if needed
+            val dialogImage: ImageView = dialogView.findViewById(R.id.dialogimage)
+            val dialogMessage: TextView = dialogView.findViewById(R.id.dialog)
+            val dialogButton: Button = dialogView.findViewById(R.id.dialog_button)
+
+            // Set an onClickListener to the button in the custom layout
+            dialogButton.setOnClickListener {
+                // Dismiss the dialog when button is clicked
+                dialog.dismiss()  // Now you can call dismiss() on the dialog object
+
+                val intent = Intent(this, PaymentActivity::class.java)
+                startActivity(intent)
+            }
+
+            // Show the dialog
+            dialog.show()
+        }
+
+
     }
+
 }
