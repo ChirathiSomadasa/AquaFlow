8277a3ebc930dbb4133df9dc4e3710d9c86961d0
IT22309624 <129936797+IT22309624@users.noreply.github.com>
2024-09-22 17:49:34 +0530

    notification Type package

---
+++ app/src/main/java/com/chirathi/aquaflow/NotificationType/UrgentNoticeActivity.kt
@@ -0,0 +1,12 @@
+package com.chirathi.aquaflow.NotificationType
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chirathi.aquaflow.R
+
+class UrgentNoticeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_urgent_notice)
+    }
+}
