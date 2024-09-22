8891203202901d8bc69bafe25839b05ee836ff7d
IT22309624 <129936797+IT22309624@users.noreply.github.com>
2024-09-22 16:22:34 +0530

    NotificationManagementActivity update

--- app/src/main/java/com/chirathi/aquaflow/NotificationManagementActivity.kt
+++ app/src/main/java/com/chirathi/aquaflow/NotificationManagementActivity.kt
@@ -2,10 +2,25 @@ package com.chirathi.aquaflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.text.HtmlCompat
 
 class NotificationManagementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification_management)
+
+        val back= findViewById<ImageView>(R.id.backbtn)
+        back.setOnClickListener {
+            finish()
+        }
+
+        // Find the Button and set the HTML formatted text
+        val btnReminder = findViewById<Button>(R.id.btnReminder) // find the button by ID
+        btnReminder.text = HtmlCompat.fromHtml(getString(R.string.reminder_water_supply), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        // Set the Urgent Notice button text
+        val btnUrgentNotice = findViewById<Button>(R.id.btnUrgentNotice) // Assuming there's a separate button
+        btnUrgentNotice.text = HtmlCompat.fromHtml(getString(R.string.urgent_notice_water_supply_disruption), HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
 }
