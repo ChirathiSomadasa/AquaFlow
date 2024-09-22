55e01ad83ded8bf3541f672b34b8a0c7aafae69b
IT22309624 <129936797+IT22309624@users.noreply.github.com>
2024-09-22 10:47:54 +0530

    .

---
+++ app/src/main/java/com/chirathi/aquaflow/SupplierNotificationManagementActivity.kt
@@ -0,0 +1,11 @@
+package com.chirathi.aquaflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SupplierNotificationManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_supplier_notification_management)
+    }
+}
