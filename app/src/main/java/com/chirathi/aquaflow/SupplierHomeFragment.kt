2df2c05e0f6ede928f1b9e7559f346acf4a61b79
Balasooriya B.M.P.U <piumiudeshika02@gmail.com>
2024-10-03 02:37:11 +0530

    Revert "update"
    This reverts commit c132875fcf43d7211ea8de78357db160a780767f, reversing
    changes made to 4981da6f88943a6f0b37ea9816915148beb7c49b.

--- app/src/main/java/com/chirathi/aquaflow/SupplierHomeFragment.kt
+++ app/src/main/java/com/chirathi/aquaflow/SupplierHomeFragment.kt
@@ -1,12 +1,10 @@
 package com.chirathi.aquaflow
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 
 class SupplierHomeFragment : Fragment() {
 
@@ -16,28 +14,6 @@ class SupplierHomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_supplier_home, container, false)
-        val view =  inflater.inflate(R.layout.fragment_supplier_home, container, false)
-
-        val NotificationManagementView = view.findViewById<Button>(R.id.Notification_Management_btn)
-        NotificationManagementView.setOnClickListener {
-            val intent = Intent(context, NotificationManagementActivity::class.java)
-            startActivity(intent)
-        }
-
-        val DeliverySchedulesView = view.findViewById<Button>(R.id.Delivery_Schedules_btn)
-        DeliverySchedulesView.setOnClickListener {
-            val intent = Intent(context, NotificationManagementActivity::class.java)
-            startActivity(intent)
-        }
-
-        val scanqrcodeView = view.findViewById<Button>(R.id.Scan_QR_code_btn)
-        scanqrcodeView.setOnClickListener {
-            val intent = Intent(context, ScanQRCodeActivity::class.java)
-            startActivity(intent)
-        }
-
-        return view
-
     }
 
 }
