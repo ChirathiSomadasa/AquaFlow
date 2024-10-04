07f390b316d0ea6b766703e2c7cfac2ff406b652
Balasooriya B.M.P.U <piumiudeshika02@gmail.com>
2024-10-04 14:16:42 +0530

    update

--- app/src/main/java/com/chirathi/aquaflow/HomeFragment.kt
+++ app/src/main/java/com/chirathi/aquaflow/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.chirathi.aquaflow
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class HomeFragment : Fragment() {
 
@@ -13,7 +15,15 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val scanqrcodeView = view.findViewById<Button>(R.id.view_more_quota)
+        scanqrcodeView.setOnClickListener {
+            val intent = Intent(context, WaterQuotaActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
 }
