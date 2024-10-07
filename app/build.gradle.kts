d569dff7dfc4b83a1c4ee4029792934f01c53fa1
Chirathi Somadasa <chirathisomadasa@gmail.com>
2024-10-07 18:45:34 +0530

    Implement upload profile image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
     kotlinOptions {
         jvmTarget="1.8"
     }
+
 }
 
 dependencies {
@@ -47,6 +48,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("com.google.firebase:firebase-firestore:25.1.0")
     implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-storage:21.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -56,5 +58,8 @@ dependencies {
     implementation ("com.github.androidmads:QRGenerator:1.0.1")
     implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation ("com.google.zxing:core:3.4.1")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
 
 }
