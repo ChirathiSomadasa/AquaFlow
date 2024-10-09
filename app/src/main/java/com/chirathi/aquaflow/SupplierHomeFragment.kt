9ef20b4fb92d702722d4748d24c4eab7e2b2c6bc
Balasooriya B.M.P.U <piumiudeshika02@gmail.com>
2024-10-09 19:33:19 +0530

    update

--- app/src/main/java/com/chirathi/aquaflow/SupplierHomeFragment.kt
+++ app/src/main/java/com/chirathi/aquaflow/SupplierHomeFragment.kt
@@ -2,13 +2,21 @@ package com.chirathi.aquaflow
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SupplierHomeFragment : Fragment() {
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +25,36 @@ class SupplierHomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_supplier_home, container, false)
 
+        firestore = Firebase.firestore
+        auth = FirebaseAuth.getInstance()
+
+        // Reference to profile name and email TextViews
+        val profileName = view.findViewById<TextView>(R.id.userNameTextView) // Add this
+
+        // Fetch current user data from Firebase
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val userId = currentUser.uid
+
+            // Fetch user data from Firestore
+            firestore.collection("users").document(userId)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        // Set user data to UI
+                        val firstName = document.getString("firstName") ?: ""
+                        val lastName = document.getString("lastName") ?: ""
+
+                        // Concatenate first and last names and display them
+                        profileName.text = "Hello $firstName $lastName"
+
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // Handle errors
+                    Log.e("ProfileFragment", "Error fetching user data", exception)
+                }
+        }
 
         val NotificationManagementView = view.findViewById<Button>(R.id.Notification_Management_btn)
         NotificationManagementView.setOnClickListener {
