97dbb3b459e068567d7c7b6f9dc607e8640605c8
HirushiniChandrasoma <HirushiniRashmika.C@gmail.com>
2024-10-10 09:34:43 +0530

    back to home button included

--- app/src/main/java/com/chirathi/aquaflow/ProfileFragment.kt
+++ app/src/main/java/com/chirathi/aquaflow/ProfileFragment.kt
@@ -2,26 +2,31 @@ package com.chirathi.aquaflow
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
-
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         // Inflate the layout for this fragment
-
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
         val profileView = view.findViewById<RelativeLayout>(R.id.profile_details)
-        // Set a click listener on the profile details layout
         profileView.setOnClickListener {
             // Start ProfileDetailsActivity
             val intent = Intent(activity, ProfileDetailsActivity::class.java)
@@ -33,10 +38,22 @@ class ProfileFragment : Fragment() {
             val intent = Intent(context, QRCodeActivity::class.java)
             startActivity(intent)
         }
-        return view
 
+        val logoutView = view.findViewById<RelativeLayout>(R.id.logout_button) 
+        logoutView.setOnClickListener {
+            performLogout()
+        }
+
+        return view
     }
 
+    private fun performLogout() {
+        auth.signOut() // Sign out from Firebase
+        Toast.makeText(context, "Logged out successfully.", Toast.LENGTH_SHORT).show()
 
-
+        // Navigate to LoginActivity
+        val intent = Intent(activity, LoginActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish() // Finish the current activity
+    }
 }
