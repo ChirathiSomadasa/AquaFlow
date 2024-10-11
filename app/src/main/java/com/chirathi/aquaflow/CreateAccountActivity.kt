e3a4828d3dba7bad35234212ac9c822af142c1a9
Chirathi Somadasa <chirathisomadasa@gmail.com>
2024-10-11 10:57:07 +0530

    Update

--- app/src/main/java/com/chirathi/aquaflow/CreateAccountActivity.kt
+++ app/src/main/java/com/chirathi/aquaflow/CreateAccountActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 
 class CreateAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +24,13 @@ class CreateAccountActivity : AppCompatActivity() {
             startActivity(intent)
         }*/
 
-        val consumer = findViewById<Button>(R.id.consumer_btn) // consumer_layout
+        val consumer = findViewById<LinearLayout>(R.id.consumer_layout) // consumer_layout
         consumer.setOnClickListener {
             val intent = Intent(this, CustomerRegistrationActivity::class.java)
             intent.putExtra("userType", "Consumer")
             startActivity(intent)
         }
-        val supplier = findViewById<Button>(R.id.supplier_btn) // supplier_layout
+        val supplier = findViewById<LinearLayout>(R.id.supplier_layout) // supplier_layout
         supplier.setOnClickListener {
             val intent = Intent(this, SupplierRegistrationActivity::class.java)
             intent.putExtra("userType", "Supplier")
